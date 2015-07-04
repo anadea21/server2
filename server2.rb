6a7cb84e49a76ca198f9618a85ffa33446b8dbee
Ana De Aza <anadea21@hotmail.com>
2015-07-04 14:28:17 -0400

    added server2 file

---
+++ server2.rb
@@ -0,0 +1,30 @@
+
+
+# require 'sinatra'
+
+# get '/' do
+
+#  "Hello world"
+
+
+# get "/sinatra" do
+
+# "Hello Sinatra"
+
+# end
+
+# end
+
+require 'sinatra'
+
+get '/' do
+	"Hello World"
+end
+
+# get '/blah' do
+#   "Hello World"
+# end
+
+get '/sinatra' do
+  "Hello Sinatra"
+end
